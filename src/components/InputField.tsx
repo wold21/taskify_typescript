fc7f0b3dbcec10b938d4883904e99805f2e26085
wold21 <wold2180@gmail.com>
2022-06-06 15:51:24 +0900

    todo list 추가 및 수정 삭제

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import'./style.css';
 
 interface Props {
@@ -7,16 +7,22 @@ interface Props {
     handleAdd: (e: React.FormEvent) => void;
 }
 const InputField: React.FC<Props> = ({todo, setTodo, handleAdd}) => {
-  return (
-    <form action="" className="input" onSubmit={handleAdd}>
-        <input type="input"
-        value={todo}
-        onChange={
-            (e) => setTodo(e.target.value)
-        }
-        placeholder='Enter a task' className='input__box'/>
-        <button type="submit" className="input_submit">Go</button>
-    </form>
-)}
+    const inputRef = useRef<HTMLInputElement>(null);
+    return (
+        <form action="" className="input" onSubmit={(e) => {
+                handleAdd(e)
+                inputRef.current?.blur()
+            }}>
+            <input 
+                ref={inputRef}
+                type="input"
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}
+                placeholder='Enter a task' className='input__box'
+            />
+            <button type="submit" className="input_submit">Go</button>
+        </form>
+    );
+};
 
 export default InputField
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Todo } from '../model';
+import SingleTodo from './SingleTodo';
+import './style.css';
+
+interface Props{
+    todos:Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList:React.FC<Props> = ({todos, setTodos}) => {
+    return( 
+        <div className="todos">
+            {todos?.map((todo)=>(
+                <SingleTodo
+                    todos={todos}
+                    todo={todo}
+                    key={todo.id}
+                    setTodos={setTodos}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default TodoList;
