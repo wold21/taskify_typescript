25d0351af3f873cdcbed95a8825600c31d587a6b
wold21 <wold2180@gmail.com>
2022-06-15 22:24:36 +0900

    reducer intro

--- src/model.ts
+++ src/model.ts
@@ -3,3 +3,9 @@ export interface Todo {
     todo:string;
     isDone:boolean;
 }
+
+type Actions = 
+| {type:'add', payload:string} 
+| {type:'remove', payload:number}
+| {type:'done', payload:number}
+
